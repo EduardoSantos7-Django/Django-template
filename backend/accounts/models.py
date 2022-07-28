7076b0e00cffb7b3f22cf950fcd9ce4c2ca1607a
Eduardo-Santos7 <eduardoarcanjo607@gmail.com>
2022-07-28 01:40:11 -0300

    🐛 Fix create_user flow

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -12,12 +12,8 @@ def user_pk():
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password, **extra_fields):
-        """Create and save a user with the given, email, and password."""
-
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a user with the given, email and password."""
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -25,8 +21,13 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email=None, password=None, **extra_fields):
-        """Create and save a superuser with the given, email, and password."""
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -35,7 +36,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def with_perm(
         self, perm, is_active=True, include_superusers=True, backend=None, obj=None
